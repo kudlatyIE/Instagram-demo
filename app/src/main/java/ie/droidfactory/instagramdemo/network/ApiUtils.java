d12e15033c05b99cb215d09f9e4fcec49f08056b
Nikodem Walicki <walicki@gmail.com>
2018-07-08 10:45:17 +0100

    Revert "redirect url and client ID update"

--- app/src/main/java/ie/droidfactory/instagramdemo/network/ApiUtils.java
+++ app/src/main/java/ie/droidfactory/instagramdemo/network/ApiUtils.java
@@ -2,19 +2,17 @@ package ie.droidfactory.instagramdemo.network;
 
 public class ApiUtils {
 
-    private final static String REDIRECT="&redirect_uri=";
-    private final static String RESPONSE_TYPE="&response_type=token";
     private final static String SEVICE_AUTH_ENDPOINT_START =
         "https://api.instagram.com/oauth/authorize/?client_id=";
-//    private final static String SEVICE_AUTH_ENDPOINT_END =
-//            "&redirect_uri=https://kudlatyie.github.io&response_type=token";
+    private final static String SEVICE_AUTH_ENDPOINT_END =
+            "&redirect_uri=https://kudlatyie.github.io&response_type=token";
     static final String PARAM_ACCESS_TOKEN = "access_token";
 
     public static final String ENDPOINT_SELF = "https://api.instagram.com/v1/users/self/";
     static final String URL_RECENT_MEDIA = "media/recent/";
 
-    public static String getAuthUrl(String redirectUrl, String clientId){
+    public static String getAuthUrl(String clientId){
         if(null==clientId) return null;
-        else return SEVICE_AUTH_ENDPOINT_START.concat(clientId).concat(REDIRECT.concat(redirectUrl).concat(RESPONSE_TYPE).concat(clientId));
+        else return SEVICE_AUTH_ENDPOINT_START.concat(clientId).concat(SEVICE_AUTH_ENDPOINT_END);
     }
 }
